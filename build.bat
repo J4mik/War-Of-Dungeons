a3d76ee0ea546c9f2a4577f2058f54b9935d6e8a
J4mik <j4mikl@gmail.com>
2026-03-28 09:30:50 +0000

    modulating alpha using fragment shader

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+glslc -fshader-stage=fragment shaders/fragment.glsl -o shaders/fragment.spv
+glslc -fshader-stage=vertex shaders/vertex.glsl -o shaders/vertex.spv
+
 cmake -S . -B bin -G Ninja 
 cd bin
 ninja -j4
